26ccae18f40493f5c8be7eb3a291febbd17dcfb5
Nurbek12 <rustamovnurbek02@gmail.com>
2024-01-13 12:09:33 +0500

    removed not used var 1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createWebHistory, createRouter } from 'vue-router'
-import store from '../store'
+// import store from '../store'
 
 export default createRouter({
     history: createWebHistory(),
